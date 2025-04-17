0f5bacf10463f665e0f92bc47e82db1cda630181
Jenica B Sarasam <am.sc.u4cse23323@am.students.amrita.edu>
2025-04-17 18:42:08 +0530

    functionality

--- lib/screen/onboarding_screen.dart
+++ lib/screen/onboarding_screen.dart
@@ -1,4 +1,6 @@
 import 'package:ai_assistant/helper/global.dart';
+import 'package:ai_assistant/model/onboard.dart';
+import 'package:ai_assistant/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -7,15 +9,29 @@ class OnboardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final c=PageController();
+    final list=[
+      //onboarding 1
+      Onboard(
+        title: 'Ask me Anything', subtitle: 'I can be your best friend and I will help you !', lottie: 'ask-me'),
+        //onboarding 2
+        Onboard(
+          title: 'Imagination to Reality', subtitle: 'Just tell me , I will create something awesome!', lottie: 'hai')
+    ];
     return Scaffold(
-      body: Column(
+      body: PageView.builder(
+        controller: c,
+        itemCount: list.length,
+        itemBuilder: (ctx, ind){
+          final isLast = ind==list.length -1;
+        return Column(
         children: [
           //lottie
-          Lottie.asset('assets/lottie/ask-me.json',height: mq.height*.6),
+          Lottie.asset('assets/lottie/${list[ind].lottie}.json',height: mq.height*.6),
           //title
-          const Text(
-            'Ask me Anything',
-            style: TextStyle(
+          Text(
+            list[ind].title,
+            style: const TextStyle(
               fontSize: 24, fontWeight: FontWeight.w900, letterSpacing: .65
             ),
           ),
@@ -24,10 +40,10 @@ class OnboardingScreen extends StatelessWidget {
           //subtitle
           SizedBox(
             width: mq.width * .7,
-            child: const Text(
-              'I can be your best friend and I will help you !',
+            child: Text(
+              list[ind].subtitle,
               textAlign: TextAlign.center,
-              style: TextStyle(
+              style: const TextStyle(
                 fontWeight: FontWeight.w600,
                 fontSize: 15, letterSpacing: .65, color: Color.fromARGB(255, 5, 1, 20),
                 
@@ -38,12 +54,12 @@ class OnboardingScreen extends StatelessWidget {
           //dots    
           Wrap(
             spacing: 14,
-            children: List.generate(2, (i)=>Container(
-              width: 16, 
+            children: List.generate(list.length, (i)=>Container(
+              width: i==ind ?  20 : 16 , 
               height: 8,
-              decoration:const BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.all(Radius.circular(5))),
+              decoration:BoxDecoration(
+                color: i==ind ? Colors.blue :Colors.grey,
+                borderRadius: const BorderRadius.all(Radius.circular(5))),
             )),
           ),
           const Spacer(),
@@ -58,13 +74,22 @@ class OnboardingScreen extends StatelessWidget {
               ),
               minimumSize: Size(mq.width * .6, 60)
             ),
-            onPressed: (){},
-            child: const Text('Next',
-            style: TextStyle(color: Colors.white,),
+            onPressed: (){
+              if(isLast){
+                Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_)=>const HomeScreen()));
+              }
+              else{
+                c.nextPage(duration: const Duration(milliseconds: 700), curve: Curves.ease);
+              }
+            },
+            child: Text(
+              isLast ? 'Finish' : 'Next',
+            style: const TextStyle(color: Colors.white,),
             )) , 
           const Spacer(flex: 2),
         ],
-      ),
+      );
+      })
     );
   }
 }
