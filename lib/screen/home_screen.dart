b05f1d894e181fd4392627565d1d7be528e45f70
Jenica B Sarasam <am.sc.u4cse23323@am.students.amrita.edu>
2025-04-18 15:20:34 +0530

    home screen

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:ai_assistant/helper/global.dart';
 import 'package:ai_assistant/helper/pref.dart';
+import 'package:ai_assistant/widget/home_cart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -21,9 +22,37 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     //Initializing device size
     mq=MediaQuery.sizeOf(context);
-    return const Scaffold(
-      body: Center(
-        child: Text("Welcome!")
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 2,
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        title: const Text(
+          appName,
+          style: TextStyle(
+            color: Color.fromARGB(255, 19, 88, 145),fontSize: 20,fontWeight: FontWeight.w500
+          ),
+        ),
+        //actions
+        actions: [
+          IconButton(
+            padding: const EdgeInsets.only(right: 10),
+            onPressed: (){},
+            icon: const Icon(
+              Icons.brightness_4_rounded,
+              color: Color.fromARGB(255, 4, 63, 110),
+              size: 26,
+            ))
+        ],
+
+      ),
+      //body
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: mq.width * 0.04,
+          vertical: mq.height * 0.04
+          ),
+        children: const [HomeCart()],
       ),
     );
   }
